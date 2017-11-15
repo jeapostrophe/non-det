5053d73a6f238ebb60003f72816bc667ec7d97d7
Jay McCarthy <jay.mccarthy@gmail.com>
2017-11-14 20:35:10 -0500

    New evaluation strategy

--- main.rkt
+++ main.rkt
@@ -3,6 +3,7 @@
                      syntax/parse)
          syntax/parse/define
          racket/hash
+         racket/format
          racket/struct
          racket/match
          racket/list
@@ -62,7 +63,6 @@
        (*theory (theory-flatten (list rules ...))))]))
 
 (struct state (env goals proofs) #:transparent)
-(struct state-tree (active solutions) #:transparent)
 
 ;; XXX In these functions, it would be nice to know what was already
 ;; ground so we didn't always allocate and rebuild these things
@@ -98,98 +98,258 @@
   ;; disconnected, because they are constraints on free variables
   (hash-empty? (state-goals st)))
 
-(define (solve/tree the-thy st #:done? done?)
-  (match-define (*theory thy-rules) the-thy)
-  (match-define (state-tree active sols) st)
-  (printf "ACTIVE ~a // SOLS ~a\n"
-          (length active) (length sols))
-  (for*/fold
-      ([a-rule-succ? #f]
-       [new-active empty]
-       [new-sols sols]
-       #:result
+;; Steps
+(struct *query (thy extract step))
+(struct step:next-state (in out) #:transparent)
+(struct step:init-goalq (st:in st:out st st-progress?) #:transparent)
+(struct step:next-goal1 (st:in st:out st st-progress? g:in g:br) #:transparent)
+(struct step:next-rule1 (st:in st:out st st-progress? g:in g:br ag agt r:in r:out) #:transparent)
+(struct step:next-goalN (st:in st:out st st-progress? g:br) #:transparent)
+(struct step:next-ruleN (st:in st:out st st-progress? g:br ag agt r:in) #:transparent)
+(struct step:end-state (st:in st:out st st-progress?) #:transparent)
+(struct step:solution (sol next) #:transparent)
+(struct step:done () #:transparent)
+
+(define (format-st st)
+  (match-define (state env goals proofs) st)
+  (~a "S("(hash-count goals)")"))
+(define format-step
+  (match-lambda
+    [(step:next-state in out)
+     (~a "1: next-state " (length in) " -> " (length out))]
+    [(step:init-goalq st:in st:out st st-progress?)
+     (~a "2: init-goalq " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?)]
+    [(step:next-goal1 st:in st:out st st-progress? g:in g:br)
+     (~a "3: next-goal1 " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?
+         " G(" (length g:in) ", " (length g:br) ")")]
+    [(step:next-rule1 st:in st:out st st-progress? g:in g:br ag agt r:in r:out)
+     (~a "4: next-rule1 " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?
+         " G(" (length g:in) ", " (length g:br) ")"
+         " " ag "=" agt
+         " R(" (length r:in) ", " (length r:out) ")")]
+    [(step:next-goalN st:in st:out st st-progress? g:br)
+     (~a "5: next-goalN " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?
+         " GBR(" (length g:br) ")")]
+    [(step:next-ruleN st:in st:out st st-progress? g:br ag agt r:in)
+     (~a "6: next-ruleN " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?
+         " GBR(" (length g:br) ")"
+         " " ag "=" agt
+         " R(" (length r:in) ")")]
+    [(step:end-state st:in st:out st st-progress?)
+     (~a "7: end-state " (length st:in) " -> " (length st:out)
+         " / " (format-st st) " " st-progress?)]
+    [(step:solution sol next)
+     (~a "8: solution " (~e sol) ", " (format-step next))]
+    [(step:done)
+     (~a "9: done")]))
+(define (show-step s)
+  (displayln (format-step s))
+  (flush-output))
+
+(define (step q)
+  (match-define (*query thy extract s) q)
+  (match-define (*theory rules) thy)
+  (define sp
+    (match s
+      ;; 1) Select a state to look at
+      [(step:next-state in out)
+       (match in
+         ;; If there is one, then start inspecting it
+         [(cons next in)
+          (step:init-goalq in out next #f)]
+         ;; If there are no more states in the queue, then
+         ['()
+          (if (empty? out)
+            ;; If there are no more period, then stop
+            (step:done)
+            ;; Otherwise, start again from the beginning
+            (step:next-state (reverse out) empty))])]
+      ;; 2) Initialize the goal queue
+      [(step:init-goalq st:in st:out st st-progress?)
+       (match-define (state env goals proofs) st)
+       (step:next-goal1 st:in st:out st st-progress? (hash-keys goals) empty)]
+      ;; 3) Work on goals by applying them to rules. If a goal has
+      ;; multiple options, look at it later
+      [(step:next-goal1 st:in st:out st st-progress? g:in g:br)
+       (match g:in
+         [(cons active-goal g:in)
+          (match-define (state env goals proofs) st)
+          ;; If there is a goal, then start trying to apply each rule
+          (step:next-rule1 st:in st:out st st-progress?
+                           g:in g:br
+                           active-goal
+                           (hash-ref goals active-goal)
+                           rules empty)]
+         ['()
+          ;; Otherwise, switch and start working on goals with branches
+          (step:next-goalN st:in st:out st st-progress? g:br)])]
+      ;; 4) Consider each rule for the goal
+      [(step:next-rule1 st:in st:out st st-progress? g:in g:br
+                        active-goal active-goal-term r:in r:out)
+       (match r:in
+         ;; If there are rules left, then try to apply one
+         [(cons active-rule r:in)
+          (match (apply-rule active-rule active-goal-term)
+            ;; If the rule doesn't apply, then move on
+            ;;
+            ;; XXX May want to remember that it was a guard that failed
+            ;; and try this later
+            [(or (rule-fail) (rule-guard-fail))
+             (step:next-rule1 st:in st:out st st-progress? g:in g:br
+                              active-goal active-goal-term r:in r:out)]
+            ;; If it does apply then...
+            [(? rule-succ? succ)
+             (if (empty? r:out)
+               ;; If this is the first success, remember it
+               (step:next-rule1 st:in st:out st st-progress? g:in g:br
+                                active-goal active-goal-term
+                                r:in (list succ))
+               ;; If this is the second, then move this goal to the
+               ;; branch queue, we'll come back to it later
+               (step:next-goal1 st:in st:out st st-progress? g:in
+                                (cons active-goal g:br)))])]
+         ;; If the are no more rules to apply, then
+         ['()
+          ;; If there were any successes...
+          (match r:out
+            ;; If there were none
+            ['()
+             (step:next-goal1 st:in st:out st st-progress? g:in g:br)]
+            [(list succ)
+             (step:next-goal1 st:in st:out (apply-succ st active-goal succ) #t
+                              g:in g:br)])])]
+      ;; 5) Now go back and fork on the goals with multiple options
+      ;; (note, that they may not be multiple options any more because
+      ;; an earlier single rule could have eliminated the one or more
+      ;; of the options---that's why we didn't save the matcher
+      ;; results)
+      [(step:next-goalN st:in st:out st st-progress? g:br)
+       (match g:br
+         [(cons active-goal g:br)
+          (match-define (state env goals proofs) st)
+          ;; If there is a goal, then start trying to apply each rule
+          (step:next-ruleN st:in st:out st st-progress?
+                           g:br
+                           active-goal (hash-ref goals active-goal)
+                           rules)]
+         ['()
+          ;; Otherwise, finalize looking at a state
+          (step:end-state st:in st:out st st-progress?)])]
+      ;; 6) Consider each rule for branching goals
+      [(step:next-ruleN st:in st:out st st-progress? g:br ag agt r:in)
+       (match r:in
+         ;; If there is a rule, try to apply it
+         [(cons active-rule r:in)
+          (match (apply-rule active-rule agt)
+            ;; If the rule doesn't apply, then move on
+            ;;
+            ;; XXX May want to remember that it was a guard that failed
+            ;; and try this later
+            [(or (rule-fail) (rule-guard-fail))
+             (step:next-ruleN st:in st:out st st-progress? g:br ag agt r:in)]
+            ;; If it does apply then... and create a new child state
+            [(? rule-succ? succ)
+             (step:next-ruleN st:in (cons (apply-succ st ag succ) st:out)
+                              ;; Because we are forking the state
+                              ;; here, any progress that we made will
+                              ;; be preserved in the forked state, so
+                              ;; we set st-progress back to #f
+                              st #f g:br ag agt r:in)])]
+         ;; If there are no more rules, then go to next goal
+         ['()
+          (step:next-goalN st:in st:out st st-progress? g:br)])]
+      ;; 7) When we are done looking at a state, then decide if we will
+      ;; return it as a solution or put it on the out queue
+      [(step:end-state st:in st:out st st-progress?)
        (cond
-         ;; If there was a change
-         [a-rule-succ?
-          (or
-           ;; XXX We could wrap up the state so we can resume in the
-           ;; future
-           (done? new-sols)
-           (solve/tree the-thy (state-tree new-active new-sols)
-                       #:done? done?))]
+         [(solution? st)
+          (step:solution st (step:next-state st:in st:out))]
+         [st-progress?
+          (step:next-state st:in (cons st st:out))]
          [else
-          ;; We never finished and now rule applies, so there's no
-          ;; work to be done.
-          #f]))
-      (;; Look at every single state (i.e. the fringe of the search space)
-       [s (in-list active)]
-       ;; For each goal, try to advance it
-       [(g gc) (in-hash (state-goals s))]
-       ;; For each rule, try to apply it
-       [r (in-list thy-rules)])
+          (step:next-state st:in st:out)])]
+      ;; 8) If we are given a solution, throw it away and go to the next
+      [(step:solution sol next)
+       next]
+      ;; 9) If we are done, then return
+      [(step:done)
+       s]))
+  (show-step sp)
+  (*query thy extract sp))
 
-    ;; XXX Maybe add a special case where we look at each goal and see
-    ;; if only one rule applies and then apply it, because we will
-    ;; have to go through that at some point. (If one rule applies and
-    ;; derives false, this should be an early quit too.) [However, if
-    ;; this rule diverges and another one would get to false, then we
-    ;; will be stuck, so there needs to be a way to balance them out
-    ;; and try different ones ---- maybe need a hash of goals and a
-    ;; queue of them?]
+(define (apply-succ st active-goal succ)
+  (match-define (state env goals proofs) st)
+  (match-define (rule-succ proof more-goals more-subst) succ)
+  (apply-subst
+   more-subst
+   (state env
+          ;; XXX Detect if any more-goals has already been seen
+          ;; XXX Detect if :false is one of the new goals?
+          (hash-union (hash-remove goals active-goal) more-goals)
+          (hash-set proofs active-goal proof))))
+(define (apply-rule r t)
+  (match-define (*rule name matcher) r)
+  (matcher t))
+(define (query-return? q)
+  (match-define (*query thy extract step) q)
+  (or (step:done? step)
+      (step:solution? step)))
+(define (query-done? q)
+  (match-define (*query thy extract step) q)
+  (or (step:done? step)))
+(define (extract-solution q)
+  (match-define (*query thy extract step) q)
+  (match-define (step:solution sol k) step)
+  (extract sol))
 
-    ;; XXX If two rules R1 and R2 apply in the same state, then there
-    ;; will be two next states---R1 o R2 and R2 o R1---but most of the
-    ;; time these are identical, so it is a waste of search
-    ;; space. They should be separate if neither adds a substitution
-    ;; and they don't use the same goal. (Or if it adds a subst but
-    ;; there's no effect on R2)
+;; Main interface := Construct a query, step until you a reach a
+;; solution, potentially ask for more solutions.
+(define (query #:theory [the-thy empty-theory]
+               #:extract [extract (λ (x) x)]
+               #:goal initial-goal)
+  (define initial-goal-id (gensym 'initial))
+  (define initial-state
+    (state (hasheq) (hasheq initial-goal-id initial-goal) (hasheq)))
+  (*query the-thy extract
+          (step:next-state (list initial-state) empty)))
 
-    (match-define (state env goals proofs) s)
-    (match-define (*rule name matcher) r)
-    (match (matcher gc)
-      [(or (rule-fail)
-           (rule-guard-fail))
-       (values a-rule-succ? new-active new-sols)]
-      [(rule-succ proof more-goals more-subst)
-       (define s-p
-         (apply-subst
-          more-subst
-          (state env
-                 ;; XXX Detect if any more-goals has already been seen
-                 ;; XXX Detect if :false is one of the new goals?
-                 (hash-union (hash-remove goals g) more-goals)
-                 (hash-set proofs g proof))))
-       (if (solution? s-p)
-         (values #t new-active (cons s-p new-sols))
-         (values #t (cons s-p new-active) new-sols))])))
+(define (solve1 q)
+  (define qp (step q))
+  (if (query-return? qp)
+    qp
+    (solve1 qp)))
 
-(define (solve* #:theory [the-thy empty-theory]
-                #:vars [query-vars '()]
-                #:goals [initial-goals '()]
-                #:count [k 1])
-  (define (done? solutions)
-    (and (<= k (length solutions))
-         (for/list ([sol (in-list solutions)])
-           (match-define (state env goals proofs) sol)
-           (for/hasheq ([k (in-list query-vars)])
-             (values (var-id k) (hash-ref env k))))))
-  (define st0
-    (state-tree
-     (list
-      (state (hasheq)
-             (for/hasheq ([g (in-list initial-goals)])
-               (values (gensym 'initial) g))
-             (hasheq)))
-     (list)))
-  (solve/tree the-thy st0 #:done? done?))
+(define (solve/k k q)
+  (cond
+    [(zero? k)
+     empty]
+    [else
+     (define qp (solve1 q))
+     (if (query-done? qp)
+       empty
+       (cons (extract-solution qp) (solve/k (sub1 k) qp)))]))
 
+(define (solve* q)
+  (solve/k +inf.0 q))
+
+(define (extract-vars vs)
+  (λ (sol)
+    (match-define (state env goals proofs) sol)
+    (for/hasheq ([k (in-list vs)])
+      (values (var-id k) (hash-ref env k)))))
 (define-simple-macro (with-vars (v:id ...) . body)
   (let ([v (var (gensym 'v))] ...) . body))
-(define-simple-macro (solve (var:id ...) goal:expr ...)
+(define-simple-macro (solve (var:id ...) goal:expr)
   (with-vars (var ...)
-    (solve* #:theory (current-theory)
-            #:vars (list var ...)
-            #:goals (list goal ...))))
+    (solve*
+     (query #:theory (current-theory)
+            #:extract (extract-vars (list var ...))
+            #:goal goal))))
 
 (define-simple-macro (define-theory x:id . body)
   (define x (theory . body)))
@@ -272,44 +432,7 @@
           (:remove x ys io)))
 
   (with-theory list-theory
-    (solve (O) (:remove 'A '(B A C) O)))
-
-  #;
-  (with-vars (O)
-    (solve* #:theory list-theory
-            #:vars (list O)
-            #:goals (list (:remove 'A '(B A C) O))
-            #:path '(remove-tail disunify-diff unify-var-lhs remove-head unify-id unify-var-rhs unify-var-lhs))))
-
-#;([// (:remove 'A '(B A C) O)]
-   => remove-tail
-   [// (:=/= A B)
-       (:== O (cons B io0))
-       (:remove 'A '(A C) io0)]
-   => disunify-diff
-   [// (:== O (cons B io0))
-       (:remove 'A '(A C) io0)]
-   => unify-var-lhs
-   [(:== O (cons B io0))
-    //
-    (:remove 'A '(A C) io0)]
-   => remove-head
-   [(:== O (cons B io0))
-    //
-    (:== 'A 'A)
-    (:== '(C) io0)]
-   => unify-id
-   [(:== O (cons B io0))
-    //
-    (:== '(C) io0)]
-   => unify-var-rhs
-   [(:== O (cons B io0))
-    //
-    (:== io0 '(C))]
-   => unify-var-lhs
-   [(:== O '(B C))
-    (:== io0 '(C))
-    //])
+    (solve (O) (:remove 'A '(B A C) O))))
 
 (module+ test
   (struct :proves (Gamma Prop) #:prefab)
@@ -328,7 +451,7 @@
           (:== A (cons X XS))
           (:append XS B Z)
           (:== C (cons X Z)))
-    
+
     (rule (assume A)
           (:proves G A)
           #:=>
@@ -348,5 +471,5 @@
           (:proves D1 B)))
 
   #;(with-theory linear-logic
-    (solve () (:proves '(A B) '(tensor A B))))
+      (solve () (:proves '(A B) '(tensor A B))))
   )
