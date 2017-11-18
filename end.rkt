5effd70adbeb1ba2b899f03c35f7f84481616ab8
Jay McCarthy <jay.mccarthy@gmail.com>
2017-11-18 08:00:52 -0500

    queue

--- end.rkt
+++ end.rkt
@@ -4,6 +4,7 @@
          racket/match
          racket/list
          racket/stream
+         racket/generic
          syntax/parse/define)
 
 ;; This implements a form of non-determinism that is similar
@@ -22,68 +23,84 @@
 
 (struct state (p kont))
 
+(define-generics queue
+  (qempty? queue)
+  (qhead queue)
+  (enq queue . v))
+
 ;; Implement a queue of problems --- and thus run with BFS ---
 ;; although we could instead use a functional priority queue and
 ;; get either DFS or something like A* if we could inspect the
 ;; states and make some decision.
-(define (solutions st:in st:out)
-  (match st:in
-    [(cons (state p k) st:in)
+(define solutions
+  (match-lambda
+    [(? qempty?) empty-stream]
+    [(app qhead (cons (state p k) q))
      (match p
        ;; Do one step of work...
        [(bind x mx)
-        (solutions st:in (list* (state x (kont:bind mx k)) st:out))]
+        (solutions (enq q (state x (kont:bind mx k))))]
        [(seq s)
-        ;; xxx if it is inf, then put a par at the end? so we do
-        ;; something like DFS --- maybe this is stupid.
         (cond
           [(stream-empty? s)
-           (solutions st:in (list* (state (fail) k) st:out))]
+           (solutions (enq q (state (fail) k)))]
           [else
-           (solutions st:in (list* (state (*par (ans (stream-first s))
-                                                (seq (stream-rest s)))
-                                          k)
-                                   st:out))])]
+           (solutions (enq q (state (*par (ans (stream-first s))
+                                          (seq (stream-rest s)))
+                                    k)))])]
        [(*par x y)
-        (solutions st:in (list* (state x (kont:par y k)) st:out))]
+        (solutions (enq q (state x (kont:par y k))))]
        ;; ...And when it ends in a result...
        [(fail)
         (match k
           ;; If it goes to a par, then ignore and choose the other
           [(kont:par y k)
-           (solutions st:in (list* (state y k) st:out))]
+           (solutions (enq q (state y k)))]
           ;; If it goes to a bind, then ignore mx and fall to k
           [(kont:bind mx k)
-           (solutions st:in (list* (state p k) st:out))]
+           (solutions (enq q (state p k)))]
           ;; If it goes to a return, then throw away the state
           [(kont:return)
-           (solutions st:in st:out)])]
+           (solutions q)])]
        [(ans a)
         (match k
           ;; When an answer goes to a par, fork the state and
           ;; duplicate the continuation
           [(kont:par y k)
-           (solutions st:in (list* (state p k) (state y k) st:out))]
+           (solutions (enq q (state p k) (state y k)))]
           ;; Call the function on a bind
           [(kont:bind mx k)
-           (solutions st:in (list* (state (mx a) k) st:out))]
+           (solutions (enq q (state (mx a) k)))]
           ;; We found a solution!
           [(kont:return)
-           (stream-cons a (solutions st:in st:out))])])]
-    ['()
-     (cond
-       [(empty? st:out)
-        empty-stream]
-       [else
-        (solutions (reverse st:out) empty)])]))
+           (stream-cons a (solutions q))])])]))
 
 (define (stream-take k s)
   (for/list ([i (in-range k)]
              [sol (in-stream s)])
     sol))
 
+(struct bfs-queue (in out)
+  #:methods gen:queue
+  [(define (qempty? q)
+     (match-define (bfs-queue in out) q)
+     (and (empty? in) (empty? out)))
+   (define (qhead q)
+     (match-define (bfs-queue in out) q)
+     (match in
+       [(cons v in)
+        (cons v (bfs-queue in out))]
+       ['()
+        (if (empty? out)
+          (error 'qhead "Queue is empty")
+          (qhead (bfs-queue (reverse out) empty)))]))
+   (define (enq q . v)
+     (match-define (bfs-queue in out) q)
+     (bfs-queue in (append v out)))])
+(define (bfs . in) (bfs-queue in empty))
+
 (define (solve p #:k [k +inf.0])
-  (stream-take k (solutions (list (state p (kont:return))) empty)))
+  (stream-take k (solutions (bfs (state p (kont:return))))))
 
 ;;;; Library
 (define-syntax (ndo stx)
