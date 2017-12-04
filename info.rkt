e26cdb7cb8152df912e239323fad8bb6b3a8b05f
Jay McCarthy <jay.mccarthy@gmail.com>
2017-12-04 13:41:52 -0500

    up

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "non-det")
-(define deps '("chk-lib" "base"))
+(define deps '("chk-lib" "base" "text-table"))
 (define build-deps '())
 (define scribblings '())
 (define version "0.0")
